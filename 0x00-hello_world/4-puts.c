063bc1c4365799d37348d7b7d50be98c096fbc18
menna <mmennatallahmohamed@gmail.com>
2023-08-10 13:29:50 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,12 +5,11 @@
  *
  * descreption: A C program that prints with put function 
  *
- * return: always 0(success)
- *
+ * return: always 0 (success)
 */
 
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
