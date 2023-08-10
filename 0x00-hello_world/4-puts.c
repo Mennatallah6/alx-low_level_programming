786a9419654283766e039b042dd05532f041f6f5
menna <mmennatallahmohamed@gmail.com>
2023-08-10 15:30:59 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
  * main - Entry point
- * description: A C program that prints with put function
- * return: Always 0 (success)
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Always 0 (success)
 */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
