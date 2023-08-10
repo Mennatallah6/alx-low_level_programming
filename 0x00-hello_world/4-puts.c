71bcfdaf4be85e8cab5a6f18d9fcbdf1377936b3
menna <mmennatallahmohamed@gmail.com>
2023-08-10 14:13:02 -0700

    task 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <studio.h>
 
 /**
- * main - entry point
- * descreption: A C program that prints with put function 
- * return: always 0 (success)
+ * main - Entry point
+ * description: A C program that prints with put function  
+ * return: Always 0 (success)
 */
 
 int main(void)
