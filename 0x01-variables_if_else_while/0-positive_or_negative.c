465aa768911e379094455b69498ac65add92af43
menna <mmennatallahmohamed@gmail.com>
2023-08-11 09:13:34 -0700

    task 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+
+int main()
+{
+
+      int x;
+      scanf("%d",&x);
+      if (x<0)
+	   printf("is negative") ;
+      if (x>0)
+	      printf("is positive");
+      else 
+	      printf("is  zero ");
+return 0;	
+ }
+
+
+
+
+
+
+
+
+
+
