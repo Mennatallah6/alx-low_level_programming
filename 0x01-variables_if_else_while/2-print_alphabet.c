e745e3ad51ebd8be5ad839c53581d869be93260c
menna <mmennatallahmohamed@gmail.com>
2023-08-14 15:33:16 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * Description: lowercase
+ * Return: 0 (success)
+*/
+int main(void)
+{
+char ch = 'a';
+while (ch <= 'Z')
+{
+putchar(ch);
+ch++;
+}
+putchar('\n');
+return (0);
+}
