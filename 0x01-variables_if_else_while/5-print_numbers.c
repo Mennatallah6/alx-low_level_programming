a3590e1e77781d6105ce0ab0b74e9642564183a0
menna <mmennatallahmohamed@gmail.com>
2023-08-14 16:38:17 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * Description: number from 0 to 10
+ * Return: 0 (success)
+*/
+int main(void)
+{
+int digit = 0;
+{
+while
+(digit <= 9);
+printf("%d", digit);
+digit++;
+}
+printf('\n);
+return (0);
+}
