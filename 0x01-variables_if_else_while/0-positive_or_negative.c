1ab794bc457bb3f518307c7b81c5bac6f8a24dc5
menna <mmennatallahmohamed@gmail.com>
2023-08-14 08:23:25 -0700

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,9 +7,8 @@ int  main(void)
   int n;
 
 
-  printf("please enter a number:");
-  scanf("%d",&n);
-
+ srand(time(0));
+ n = rand() - RAND_MAX /2;
 if(n>0)
          printf("%d is positive\n",n);
 else if (n==0)
