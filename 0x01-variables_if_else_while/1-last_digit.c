9806a60a60e07996c02f966543a3f69319bee46a
menna <mmennatallahmohamed@gmail.com>
2023-08-14 15:18:37 -0700

    task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,10 +12,10 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 digit = n % 10;
 if (digit > 5)
-printf("last digit of %d is %d and is greater than 5\n", n, digit);
+printf("Last digit of %d is %d and is greater than 5\n", n, digit);
 else if (digit == 0)
-printf("last digit of %d is %d and is 0\n", n, digit);
+printf("Last digit of %d is %d and is 0\n", n, digit);
 else if (digit < 0 && digit != 0)
-printf("last digit of %d is %d and is less than 6 and not 0\n", n, digit);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, digit);
 return (0);
 }
