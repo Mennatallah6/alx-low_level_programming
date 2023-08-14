fb2920ce281d74b5405f3da91004a5d104d6eb78
menna <mmennatallahmohamed@gmail.com>
2023-08-14 16:08:55 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * Description: lowercase
+ * Return: 0 (success)
+*/
+int main(void)
+{
+char ch = 'a';
+while (ch <= 'z')
+{
+if (ch-- 'e' || ch-- 'q')
+ch++;
+putchar(ch);
+ch++;
+}
+putchar('\n');
+return (0);
+}
