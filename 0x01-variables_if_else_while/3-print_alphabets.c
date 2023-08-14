831fc9067d740e0f04c674bdf124aa6127bdc571
menna <mmennatallahmohamed@gmail.com>
2023-08-14 15:51:49 -0700

    task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,13 +10,13 @@ int main(void)
 char ch = 'a';
 char CH = 'A';
 /*prints a - z*/
-while (ch <= 'z')
+while (ch <= 'z');
 {
 putchar(ch);
 ch++;
 }
 /*print A - Z*/
-while (CH <= 'Z')
+while (CH <= 'Z');
 {
 putchar(CH);
 CH++;
