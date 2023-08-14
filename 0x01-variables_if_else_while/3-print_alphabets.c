9f7e01c1d1c11ee9a9cf4121a732e404563922fa
menna <mmennatallahmohamed@gmail.com>
2023-08-14 15:49:01 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * Description: lowercase
+ * Return: 0 (success)
+*/
+int main(void)
+{
+char ch = 'a';
+char CH = 'A';
+/*prints a - z*/
+while (ch <= 'z')
+{
+putchar(ch);
+ch++;
+}
+/*print A - Z*/
+while (CH <= 'Z')
+{
+poutchar(CH);
+CH++;
+{
+putchar('\n');
+return (0);
