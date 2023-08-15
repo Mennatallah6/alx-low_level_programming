743f4abfa111038dcc9b60f5cb3f59d36b6d1a5a
menna <mmennatallahmohamed@gmail.com>
2023-08-15 07:15:37 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,15 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <stdio.h>
-int _putchar(char c);
-#endif
+#include" main.h"
+/**
+ * main - entry point
+ * Description: prints _putchar using putchar
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+char str[] = "_putchar";
+int ch;
+for (ch = 0; ch < 8; ch++)
+_putchar(str[ch]);
+_putchar('\n');
+return (0);
+}
