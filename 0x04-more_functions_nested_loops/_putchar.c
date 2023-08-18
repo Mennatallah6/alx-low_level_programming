4ef17fb02c5f8393a2eea47f9f8744695dab3400
menna <mmennatallahmohamed@gmail.com>
2023-08-18 10:16:25 -0700

    task 0

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,6 +1,12 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <stdio.h>
-int _putchar(char c);
-int _isupper(int c);
-#endif                
+#include <unistd.h>
+#include "main.h"
+/**
+ * _putchar - writes the character c to stout
+ * @c: the character to print 
+ * Return: on success 1.
+ * on error, -1 is returned, and errno is set appropriately.
+*/
+int _putchar(char c)
+{
+return (write(1, &c, 1)0;
+}              
